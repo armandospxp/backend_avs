0007eca077b76d250e9157f9586a2ea5ec9d8d1d
Armando Rodriguez <armandospxp@gmail.com>
2021-08-03 18:19:04 -0400

    Agregado configuraciones para el deploy en heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn backend_avs.wsgi --log-file -
--- backend_avs/settings.py
+++ backend_avs/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,6 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+#django_heroku
+django_heroku.settings(locals())
 
 # Application definition
 
