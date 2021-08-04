582cfcc4c042f414c1ca1b8f20eb8bbcddc4198d
Armando Rodriguez <armandospxp@gmail.com>
2021-08-03 23:07:35 -0400

    Cambio de vuelta en la bd

--- backend_avs/settings.py
+++ backend_avs/settings.py
@@ -103,8 +103,7 @@ import dj_database_url
 #     }
 # }
 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+DATABASES = { 'default': dj_database_url.config() }
 
 
 
