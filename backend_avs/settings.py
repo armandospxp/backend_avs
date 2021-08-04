e3db50503cbe9ae6fc17973f00bb3928e5a9cb7e
Armando Rodriguez <armandospxp@gmail.com>
2021-08-03 20:49:51 -0400

    Cambiando de vuelta a debug=True

--- backend_avs/settings.py
+++ backend_avs/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jt3o9(jzhgwtgjtnwa%1l!aob1i)(9h)zemnd0e@!povz=jm^b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
