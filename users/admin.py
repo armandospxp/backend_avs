24e3312f86e695db3de8b9e0b8487c48f971f96b
Armando Rodriguez <armandospxp@gmail.com>
2021-08-05 19:49:37 -0400

    cambios en usuarios

--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,1 @@
 """User admin classes."""
-
-# Django
-from django.contrib import admin
-
-# Models
-from users.models import User
-
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    admin.site.register(User)
