86928a8e367967b5f04702c2a2c11ec4ed1036c9
Armando Rodriguez <armandospxp@gmail.com>
2021-08-10 21:38:24 -0400

    Trabajando en users update, delete

--- users/serializers.py
+++ users/serializers.py
@@ -102,58 +102,59 @@ class UserUpdateSerializer(serializers.Serializer):
         user = User.objects.update(data)
         return user
 
-    class UserSignUpSerializer(serializers.Serializer):
-        email = serializers.EmailField(
-            validators=[UniqueValidator(queryset=User.objects.all())]
-        )
-        username = serializers.CharField(
-            min_length=4,
-            max_length=20,
-            validators=[UniqueValidator(queryset=User.objects.all())]
-        )
 
-        photo = serializers.ImageField(
-            validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])],
-            required=False
-        )
+class UserSignUpSerializer(serializers.Serializer):
+    email = serializers.EmailField(
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    username = serializers.CharField(
+        min_length=4,
+        max_length=20,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
 
-        extract = serializers.CharField(max_length=1000, required=False)
+    photo = serializers.ImageField(
+        validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])],
+        required=False
+    )
 
-        city = serializers.CharField(max_length=250, required=False)
+    extract = serializers.CharField(max_length=1000, required=False)
 
-        country = serializers.CharField(max_length=250, required=False)
+    city = serializers.CharField(max_length=250, required=False)
 
-        phone_regex = RegexValidator(
-            regex=r'\+?1?\d{9,15}$',
-            message="Debes introducir un número con el siguiente formato: +999999999. El límite son de 15 dígitos."
-        )
-        phone = serializers.CharField(validators=[phone_regex], required=False)
+    country = serializers.CharField(max_length=250, required=False)
 
-        password = serializers.CharField(min_length=8, max_length=64)
-        password_confirmation = serializers.CharField(min_length=8, max_length=64)
+    phone_regex = RegexValidator(
+        regex=r'\+?1?\d{9,15}$',
+        message="Debes introducir un número con el siguiente formato: +999999999. El límite son de 15 dígitos."
+    )
+    phone = serializers.CharField(validators=[phone_regex], required=False)
 
-        first_name = serializers.CharField(min_length=2, max_length=50)
-        last_name = serializers.CharField(min_length=2, max_length=100)
+    password = serializers.CharField(min_length=8, max_length=64)
+    password_confirmation = serializers.CharField(min_length=8, max_length=64)
 
-        def validate(self, data):
-            passwd = data['password']
-            passwd_conf = data['password_confirmation']
-            if passwd != passwd_conf:
-                raise serializers.ValidationError("Las contraseñas no coinciden")
-            password_validation.validate_password(passwd)
+    first_name = serializers.CharField(min_length=2, max_length=50)
+    last_name = serializers.CharField(min_length=2, max_length=100)
 
-            image = None
-            if 'photo' in data:
-                image = data['photo']
+    def validate(self, data):
+        passwd = data['password']
+        passwd_conf = data['password_confirmation']
+        if passwd != passwd_conf:
+            raise serializers.ValidationError("Las contraseñas no coinciden")
+        password_validation.validate_password(passwd)
 
-            if image:
-                if image.size > (512 * 1024):
-                    raise serializers.ValidationError(
-                        f"La imagen es demasiado grande, el peso máximo permitido es de 512KB y el tamaño enviado es de {round(image.size / 1024)}KB")
+        image = None
+        if 'photo' in data:
+            image = data['photo']
 
-            return data
+        if image:
+            if image.size > (512 * 1024):
+                raise serializers.ValidationError(
+                    f"La imagen es demasiado grande, el peso máximo permitido es de 512KB y el tamaño enviado es de {round(image.size / 1024)}KB")
 
-        def create(self, data):
-            data.pop('password_confirmation')
-            user = User.objects.create_user(**data)
-            return user
+        return data
+
+    def create(self, data):
+        data.pop('password_confirmation')
+        user = User.objects.create_user(**data)
+        return user
