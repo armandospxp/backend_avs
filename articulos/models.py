34ac2faf6876bc71b2a32ccc98341bd252b9ef74
Armando Rodriguez <armandospxp@gmail.com>
2021-08-12 19:36:28 -0400

    Se coloca settings para local y produccion, se corrige modelo de articulos y sus urls

--- articulos/models.py
+++ articulos/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Marca(models.Model):
-    codigo = models.CharField(primary_key=True)
+    codigo = models.CharField(primary_key=True, max_length=64)
     descripcion = models.CharField(max_length=100)
 
 
@@ -11,13 +11,13 @@ class Articulo(models.Model):
     id_articulo = models.IntegerField(primary_key=True)
     codigo = models.ForeignKey(Marca, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=80, blank=False, null=False)
-    costo = models.DecimalField(decimal_places=2)
+    costo = models.DecimalField(decimal_places=2, max_digits=32)
     porc_iva = models.PositiveIntegerField()
     porc_comision = models.IntegerField()
     stock_actual = models.PositiveBigIntegerField()
     stock_minimo = models.IntegerField()
     ultima_compra = models.DateField()
     unidad_medida = models.CharField(max_length=4)
-    precio_unitario = models.DecimalField(decimal_places=4)
-    precio_mayorista = models.DecimalField(decimal_places=4)
-    precio_especial = models.DecimalField(decimal_places=4)
+    precio_unitario = models.DecimalField(decimal_places=4, max_digits=32)
+    precio_mayorista = models.DecimalField(decimal_places=4, max_digits=32)
+    precio_especial = models.DecimalField(decimal_places=4, max_digits=32)
