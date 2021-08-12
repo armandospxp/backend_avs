125ef289a2c4754527218e686627eb15f6ecc382
Armando Rodriguez <armandospxp@gmail.com>
2021-08-11 22:06:36 -0400

    Correccion en urls general

--- backend_avs/urls.py
+++ backend_avs/urls.py
@@ -5,10 +5,11 @@ from django.urls import include, path
 from django.conf.urls.static import static
 from django.contrib import admin
 
+
 urlpatterns = [
     # Django Admin
     path('admin/', admin.site.urls),
-
+    path('articulos/', include(('articulos.urls', 'articulos'), namespace='articulos')),
     path('', include(('users.urls', 'users'), namespace='users')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
