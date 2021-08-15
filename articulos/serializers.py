0d67fdea8021e3f8bde0ea62009958471528a884
Armando Rodriguez <armandospxp@gmail.com>
2021-08-14 20:23:24 -0400

    Bigautofield para articulos

--- articulos/serializers.py
+++ articulos/serializers.py
@@ -6,7 +6,7 @@ from articulos.models import Articulo, Marca
 class ArticuloModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Articulo
-        fields = ['codigo', 'nombre', 'costo', 'porc_iva', 'porc_comision', 'stock_actual', 'stock_minimo', 'ultima_compra',
+        fields = ['id_articulo', 'codigo', 'nombre', 'costo', 'porc_iva', 'porc_comision', 'stock_actual', 'stock_minimo', 'ultima_compra',
                   'unidad_medida', 'precio_unitario', 'precio_mayorista', 'precio_especial']
 
 
