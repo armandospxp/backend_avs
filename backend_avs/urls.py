ba2b13a9a7bce6454b894057779b0aab3cf15cf9
Armando Rodriguez <armandospxp@gmail.com>
2021-08-27 11:20:07 -0400

    Se habilita simple_jwt para la generacion y actualizacion de tokens

--- backend_avs/urls.py
+++ backend_avs/urls.py
@@ -4,10 +4,10 @@ from django.conf import settings
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.contrib import admin
-# from rest_framework_simplejwt.views import (
-#     TokenObtainPairView,
-#     TokenRefreshView,
-# )
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
 urlpatterns = [
@@ -17,7 +17,7 @@ urlpatterns = [
     path('marcas/', include(('marcas.urls', 'marcas'), namespace='marcas')),
     path('roles/', include(('roles.urls', 'roles'), namespace='roles')),
     path('', include(('users.urls', 'users'), namespace='users')),
-    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
