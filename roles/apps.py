72b4e3def9bc09d2c00cdc458f3e3d2ddf868a3a
Armando Rodriguez <armandospxp@gmail.com>
2021-09-14 15:35:06 -0400

    Se trabaja por los modelos de Rol, Permiso, Modulos

--- roles/apps.py
+++ roles/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CajasConfig(AppConfig):
+class RolesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cajas'
+    name = 'roles'
