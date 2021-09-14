97588db7c6a638f1b7398a4d2c2a0384013eb117
Armando Rodriguez <armandospxp@gmail.com>
2021-09-14 15:16:22 -0400

    Se trabaja por los modelos de Rol, Permiso, Modulos

--- roles/urls.py
+++ roles/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from roles.views import GroupList, PermissionList
+from roles.views import RolList, PermissionList
 
 urlpatterns = format_suffix_patterns([
-    path('', GroupList.as_view(), name='roles'),
+    path('', RolList.as_view(), name='roles'),
     path('permisos/', PermissionList.as_view(), name='permisos'),
 ])
