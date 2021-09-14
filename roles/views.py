49d99f1e568ec4df58c7375c26355d8527c54947
Armando Rodriguez <armandospxp@gmail.com>
2021-09-14 15:10:49 -0400

    Se trabaja por los modelos de Rol, Permiso, Modulos

--- roles/views.py
+++ roles/views.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from users.serializers import GroupModelSerializer, PermisosModelSerializer
+from roles.serializers import GroupModelSerializer, PermisosModelSerializer
 
 
 class RolList(APIView):
