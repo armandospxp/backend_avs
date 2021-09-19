613cfd977b2a844c1d435f73e45501ea1d080566
Armando Rodriguez <armandospxp@gmail.com>
2021-09-18 22:49:32 -0400

    Trabajando con permisos y modulos

--- roles/urls.py
+++ roles/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from roles.views import RolList, RolDetail, PermissionList
+from roles.views import RolList, RolDetail, PermissionList, ModuloListView
 
 urlpatterns = format_suffix_patterns([
     path('roles/', RolList.as_view(), name='roles'),
     path('roles/<pk>', RolDetail.as_view(), name='roles_detalles'),
     path('permisos/', PermissionList.as_view(), name='permisos'),
+    path('modulos/', ModuloListView.as_view(), name='modulos')
 ])
