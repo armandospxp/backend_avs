5f732da9a114bc4371a011f031728d588c51ffe0
Armando Rodriguez <armandospxp@gmail.com>
2021-09-21 14:32:53 -0400

    MOdificacion modelo de ROl

--- roles/models.py
+++ roles/models.py
@@ -3,14 +3,6 @@ from django.db import models
 from users.models import User
 
 
-class Rol(models.Model):
-    id_rol = models.AutoField(primary_key=True)
-    nombre_rol = models.CharField(max_length=30)
-    usuario = models.ManyToManyField(User)
-    permiso = models.ManyToManyField(Permiso)
-    modulo = models.ManyToManyField(Modulo)
-
-
 class Modulo(models.Model):
     id_modulo = models.AutoField(primary_key=True)
     nombre_modulo = models.CharField(max_length=30)
@@ -19,3 +11,11 @@ class Modulo(models.Model):
 class Permiso(models.Model):
     id_permiso = models.AutoField(primary_key=True)
     descripcion = models.CharField(max_length=15)
+
+
+class Rol(models.Model):
+    id_rol = models.AutoField(primary_key=True)
+    nombre_rol = models.CharField(max_length=30)
+    usuario = models.ManyToManyField(User)
+    permiso = models.ManyToManyField(Permiso)
+    modulo = models.ManyToManyField(Modulo)
