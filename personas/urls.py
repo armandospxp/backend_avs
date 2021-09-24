22fd8072e3604dde35f91e7177cbde46b40664aa
Armando Rodriguez <armandospxp@gmail.com>
2021-09-24 15:43:23 -0400

    Vistas de clientes y proveedores

--- personas/urls.py
+++ personas/urls.py
@@ -8,6 +8,6 @@ urlpatterns = format_suffix_patterns([
     path('', PersonaList.as_view(), name='personas'),
     path('<int:pk>/', PerosnaDetail.as_view(), name='persona_detail'),
     # path('busqueda/', PersonaSearchViewSet, name='personasearch'),
-    path('clientes/', PersonaProveedorList.as_view(), name='personaempleado'),
-    path('proveedores/', PersonaClienteList.as_view(), name='personaproveedor'),
+    path('proveedores/', PersonaProveedorList.as_view(), name='personaempleado'),
+    path('clientes/', PersonaClienteList.as_view(), name='personaproveedor'),
 ])
