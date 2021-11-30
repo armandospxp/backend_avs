966fccea79e9c4d518e4c83b0605673924bc8486
Armando Rodriguez <armandospxp@gmail.com>
2021-11-30 18:25:08 -0300

    Se modifica el serialzer de ventas y se agrega datos de sub_total_iva, tipo_iva, nombre_articulo en detalle_venta y en venta numero_factura

--- ventas/serializers.py
+++ ventas/serializers.py
@@ -6,9 +6,23 @@ from drf_writable_nested import WritableNestedModelSerializer
 
 
 class DetalleVentaModelSerializer(serializers.ModelSerializer):
+    sub_total_iva = serializers.SerializerMethodField()
+    tipo_iva = serializers.SerializerMethodField()
+    nombre_articulo = serializers.SerializerMethodField()
+
     class Meta:
         model = DetalleVenta
         fields = '__all__'
+
+    def get_sub_total_iva(self, obj):
+        dato = (int(obj.id_articulo.porc_iva)/100)*(int(obj.id_articulo.precio_unitario)*int(obj.cantidad))
+        return str(dato)
+
+    def get_tipo_iva(self, obj):
+        return str(obj.id_articulo.porc_iva)
+
+    def get_nombre_articulo(self, obj):
+        return obj.id_articulo.nombre
 
 
 class VentaModelSerializer(WritableNestedModelSerializer, serializers.ModelSerializer):
@@ -30,6 +44,41 @@ class VentaListModelSerializer(WritableNestedModelSerializer, serializers.ModelS
     id_detalle_venta = DetalleVentaModelSerializer(many=True)
     nombre_cliente = serializers.SerializerMethodField()
     nombre_usuario = serializers.SerializerMethodField()
+    numero_factura = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Venta
+        fields = ['id_venta',
+                  'id_cliente',
+                  'id_usuario',
+                  'fecha',
+                  'total',
+                  'id_detalle_venta',
+                  'tipo_factura',
+                  'numero_factura',
+                  'nombre_cliente',
+                  'nombre_usuario'
+                  ]
+
+    def get_nombre_cliente(self, obj):
+        try:
+            dato = obj.id_cliente.nombre_apellido
+        except:
+            dato = "SIN NOMBRE"
+        return dato
+
+    def get_nombre_usuario(self, obj):
+        return obj.id_usuario.first_name + ' ' + obj.id_usuario.last_name
+
+    def get_numero_factura(self, obj):
+        return str(obj.id_usuario.configuracion.numeracion_fija_factura) + str(
+            obj.id_usuario.configuracion.numero_factura)
+
+
+class DetalleVentaListModelSerializer(WritableNestedModelSerializer, serializers.ModelSerializer):
+    id_detalle_venta = DetalleVentaModelSerializer(many=True)
+    nombre_cliente = serializers.SerializerMethodField()
+    nombre_usuario = serializers.SerializerMethodField()
 
     class Meta:
         model = Venta
@@ -43,13 +92,3 @@ class VentaListModelSerializer(WritableNestedModelSerializer, serializers.ModelS
                   'nombre_cliente',
                   'nombre_usuario'
                   ]
-
-    def get_nombre_cliente(self, obj):
-        try:
-            dato = obj.id_cliente.nombre_apellido
-        except:
-            dato = "SIN NOMBRE"
-        return dato
-
-    def get_nombre_usuario(self, obj):
-        return obj.id_usuario.first_name+' '+obj.id_usuario.last_name
