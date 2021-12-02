bde102ceb5d3e074b7779257f31106149a737901
Armando Rodriguez <armandospxp@gmail.com>
2021-12-01 22:47:24 -0300

    Se agrega campo total_nota_credito en ventas y se modifica crear nota credito para que vaya sumando el sando a favor en la venta y esto no altere la factura

--- nota_credito/views.py
+++ nota_credito/views.py
@@ -35,8 +35,11 @@ class NotaCreditoVentaView(viewsets.ModelViewSet):
             id_venta = int(datos['id_venta'])
             monto_total = int(datos['monto_total'])
             venta = get_object_or_404(Venta.objects.all(), pk=id_venta)
-            venta.total = venta.total - monto_total
-            venta.save()
+            if venta.total <= venta.total_nota_credito:
+                venta.total_nota_credito = venta.total_nota_credito + monto_total
+                venta.save()
+            else:
+                raise status.HTTP_400_BAD_REQUEST
             detalle_nota_credito = DetalleNotaCredito.objects.latest('id_detalle_nota_credito')
 
             cantidad = int(detalle_nota_credito.cantidad)
--- ventas/models.py
+++ ventas/models.py
@@ -33,3 +33,4 @@ class Venta(BaseModel):
     id_detalle_venta = models.ManyToManyField(DetalleVenta, blank=False)
     tipo_factura = models.CharField(max_length=3, choices=TIPO_FACTURA_CHOICES, default="CON")
     numero_factura_asignado = models.CharField(blank=True, null=True, max_length=30)
+    total_nota_credito = models.PositiveIntegerField(default=0)
