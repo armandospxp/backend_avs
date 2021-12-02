35adaed3b623059594a35161d6af3800a583867f
Armando Rodriguez <armandospxp@gmail.com>
2021-12-02 17:56:18 -0300

    Se agrega nombre usuario a serializers.py de arqueo caja

--- cajas/serializers.py
+++ cajas/serializers.py
@@ -4,9 +4,14 @@ from cajas.models import ArqueoCaja, MovimientoCaja
 
 
 class ArqueoCajaModelSerializer(serializers.ModelSerializer):
+    nombre_usuario = serializers.SerializerMethodField()
+
     class Meta:
         model = ArqueoCaja
         fields = '__all__'
+
+    def get_nombre_usuario(self, obj):
+        return obj.id_empleado.first_name+' '+obj.id_empleado.last_name
 
 
 class MovimientoCajaModelSerializer(serializers.ModelSerializer):
